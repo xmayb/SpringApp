b673d5ba88440aa8002d5228624d7f88c4fdf000
xmayb <denis.kozhevnikov05@gmail.com>
2025-12-23 22:57:03 -0800

    Add comments to Entity

--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -2,10 +2,10 @@ package com.example.demo.Entity;
 
 
 public class User {
-    private Long id;
+    private Long id; // Unique identifier of the user (usually primary key in DB)
     private String name;
     private String email;
-    private String password;
+    private String password;//password (should be hashed in real apps)
 
     public User(String name, String email, String password) {
         this.name = name;
@@ -18,12 +18,15 @@ public class User {
 
     }
 
+     //Partial constructor.
+    //Used when only name and email are needed
+    //  Parameterized Constructor
     public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
-
+    //Full constructor
     public User(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
@@ -32,10 +35,13 @@ public class User {
 
     }
 
+    //Getters and Setters
+    //Used to READ the identifier
     public Long getId() {
         return id;
     }
 
+    //Used when DB returns generated id or during mapping
     public void setId(Long id) {
         this.id = id;
     }
@@ -47,16 +53,24 @@ public class User {
         this.name = name;
     }
 
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
+    //Getter for password.
+    //normally you should NOT expose a password like this.
     public String getPassword() {
         return password;
     }
+
+
+     //Setter for password.
+     //Should accept already-hashed password in real systems.
     public void setPassword(String password) {
         this.password = password;
     }
